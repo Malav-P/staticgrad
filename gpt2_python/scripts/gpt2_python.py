7df45532afb897319e0fc1a9e6e1f79d6307916c
Malav_P <malavpatel2022@u.northwestern.edu>
2024-08-18 11:19:15 -0400

    update

--- gpt2_python/scripts/gpt2_python.py
+++ gpt2_python/scripts/gpt2_python.py
@@ -1,19 +1,20 @@
 import torch
-from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config, GPT2Model
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config
 import os
 import json
 import numpy as np
 import warnings
+import argparse
 
 
-class GPT2WithIntermediateStates(GPT2Model):
+class GPT2WithIntermediateStates(GPT2LMHeadModel):
     def __init__(self, config):
         super().__init__(config)
         self.intermediate_states = []
         self.module_name = None
 
     def set_hook(self, module_name):
-        named_modules = dict(model.named_modules())  # Convert to dictionary for easy lookup
+        named_modules = dict(self.named_modules())  # Convert to dictionary for easy lookup
         if module_name in named_modules:
             self.module_name = module_name
         else:
@@ -46,10 +47,7 @@ class GPT2WithIntermediateStates(GPT2Model):
         return self.intermediate_states
 
 
-def get_weights():
-
-    # Initialize the model
-    model, _ = load_model(pretrained=True, debug=False)
+def get_weights(model):
 
     # Prepare file paths
     base_path = os.path.dirname(os.path.dirname(__file__))
@@ -140,6 +138,9 @@ def yap(model, tokenizer):
     prompt = "I enjoy taking my dog out and"
     model_inputs = tokenizer(prompt, return_tensors='pt').to("cpu")
 
+    model_inputs.pop("attention_mask", None)
+
+
     sample_output = model.generate(
     **model_inputs,
     max_new_tokens=40,
@@ -153,46 +154,40 @@ def yap(model, tokenizer):
     print(generated_text)
 
 
-def gpt2_python():
-    # Initialize the model and tokenizer
-
-    model, _ = load_model()
-
+def print_modules(model):
     for name, module in model.named_modules():
         print(f"Module Name: {name}, Module Type: {module.__class__.__name__}")
 
 
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--get_weights', action='store_true', help='Extract and store GPT-2 weights')
+    parser.add_argument('--modules', action='store_true', help='GPT2 Modules')
+    parser.add_argument('--intermediate_states', action='store_true', help='Print out intermediate states')
+    parser.add_argument('--yap', action='store_true', help='Autoregressive generation of text')
+
+    args = parser.parse_args()
+    
+    model, tokenizer = load_model(pretrained=True, debug=True)
+
+    if args.get_weights:
+        get_weights(model)
+    if args.intermediate_states:
+        model.set_hook("transformer.h.0.ln_1")  # hook to the first residual layer
+        prompt = "There was a hurricane"
+
+        model_inputs = tokenizer(prompt, return_tensors='pt').to("cpu")
+        _ = model(**model_inputs)
+        intermediate_states = model.get_intermediate_states()
+        print(intermediate_states[0][0, 0, :4])  
+    if args.modules:
+        print_modules(model)
+    if args.yap:
+        yap(model, tokenizer)
+        
+
 if __name__ == "__main__":
-    # get_weights()
-
-    # model, tokenizer = load_model(pretrained=False)
-
-    # model.config.name_or_path = "gpt2"
-
-    # yap(model, tokenizer)
-
-    # base_path = os.path.dirname(__file__)
-    # weights_file = os.path.join(base_path, f"{model.config.name_or_path}_weights.bin")
-    # metadata_file = os.path.join(base_path, f"{model.config.name_or_path}_metadata.json")
-
-    # # Load weights into the model
-    # load_weights(model, weights_file, metadata_file)
-
-    # yap(model, tokenizer)
-
-
-    # model, tokenizer = load_model(pretrained=True, debug=True)
-    # model.set_hook("h.0.ln_1")
-
-    # prompt = "There was a hurricane"
-
-    # model_inputs = tokenizer(prompt, return_tensors='pt').to("cpu")
-
-    # output = model.forward(**model_inputs)
-
-    # print(model.get_intermediate_states()[0][0, 0, :4])
-
-    get_weights()
+    main()
 
 
